824d11490fd984541df169fce701f1c80feb2777
banditpig <mike_k_houghton@yahoo.co.uk>
2024-12-05 11:21:19 +0000

    tidy

--- AdventCode_2024_FSharp/src/Day5.fs
+++ AdventCode_2024_FSharp/src/Day5.fs
@@ -6,26 +6,25 @@ let loadRules (filePath: string) : string list =
   
 let loadUpdates (filePath: string)  =
    File.ReadAllLines(filePath) |> List.ofArray |> List.map (fun x -> x.Split(",") |> List.ofArray)
- 
-let applyRules rules  updates =
-   
-   let rowOk (row, pairs) =
+let rowOk rules (row, pairs) =
       //all pairs of rules are in rules
       pairs |> List.forall (fun p -> List.contains p rules )      
-
+ 
+let applyRules rules  updates =
+  
    let pairs r = 
     [ for i in 0 .. List.length r - 1 do
         for j in i + 1 .. List.length r - 1 do
             yield  $"{r.[i]}|{r.[j]}" ]
     
-   let z = updates |> List.map (fun row -> (row, pairs row))
-                   |> List.filter rowOk
+   let cnt = updates |> List.map (fun row -> (row, pairs row))
+                   |> List.filter (rowOk rules)
                    |> List.map (fun (row, _) -> row)
                    |> List.map (fun r -> r.[(List.length r) / 2])
                    |> List.map (fun r -> int r)
                    |> List.sum
 
-   printfn "Day5 Part1 %A" z
+   cnt
   
    
    
@@ -33,9 +32,8 @@ let applyRules rules  updates =
 let part1() =
    let rules = loadRules "./Data/Day5Rules.txt"
    let updates = loadUpdates "./Data/Day5Updates.txt"
-   applyRules rules updates 
-   // printfn "Day5 Part1 rules %A" (loadRules "./Data/Day5Rules.txt")
-   // printfn "Day5 Part1 updates  %A" (loadUpdates "./Data/Day5Updates.txt")
-   //  
+   let cnt = applyRules rules updates
+   printfn "Day5 Part1  %A" cnt
+ 
 let part2() =
     printfn "Day5 Part2 "
